1db3dada241848a40e2f44f88066b1afe69e382d
Mohamed Badawy <m7md.badawy@gmail.com>
2017-06-27 23:17:56 +0200

    adding notebook html

--- my_answers.py
+++ my_answers.py
@@ -4,21 +4,23 @@ from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
 import keras
+import re
 
 
-# TODO: fill out the function below that transforms the input series 
+
+# TODO: fill out the function below that transforms the input series
 # and window-size into a set of input/output pairs for use with our RNN model
 def window_transform_series(series,window_size):
     # containers for input/output pairs
     X = []
     y = []
 
-    # reshape each 
+    # reshape each
     X = np.asarray(X)
     X.shape = (np.shape(X)[0:2])
     y = np.asarray(y)
     y.shape = (len(y),1)
-    
+
     return X,y
 
 # TODO: build an RNN to perform regression on our time series input/output data
@@ -34,9 +36,16 @@ def build_part1_RNN(step_size, window_size):
 ### TODO: list all unique characters in the text and remove any non-english ones
 def clean_text(text):
     # find all unique characters in the text
+    text = text.replace('\n',' ')    # replacing '\n' with '' simply removes the sequence
+    text = text.replace('\r',' ')
+    unique = list(set(x for x in text))
+    print(unique)
+    non_english = [x for x in unique if re.match(r'[a-zA-z !,.:;?]',x) is None]
+    for x in non_english:
+        text = text.replace(x, ' ')
 
-
-    # remove as many non-english characters and character sequences as you can 
+    text = text.replace('  ',' ')
+    return text
 
 
 ### TODO: fill out the function below that transforms the input text and window-size into a set of input/output pairs for use with our RNN model
@@ -44,7 +53,10 @@ def window_transform_text(text,window_size,step_size):
     # containers for input/output pairs
     inputs = []
     outputs = []
-    
 
-    
+    for i in range(window_size,len(text),step_size):
+        x = text[i-window_size:i]
+        inputs.append(x)
+        outputs.append(text[i])
+
     return inputs,outputs
